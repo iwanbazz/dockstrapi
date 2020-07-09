7db7190c7c5564a8a292ce8f116a91fb230add02
iwanbazz <iwan.bazz@gmail.com>
2020-07-09 17:14:27 +0800

    Add plugin setting

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = {
+  graphql: {
+    endpoint: '/graphql',
+    tracing: true,
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+  },
+}
