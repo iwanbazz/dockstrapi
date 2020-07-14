ebaa1331661effa9650d8759d4e2fa66f3f45bd8
iwanbazz <iwan.bazz@gmail.com>
2020-07-14 16:02:13 +0800

    change to heroku

--- config/database.js
+++ config/database.js
@@ -1,19 +1,24 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "mongoose",
+      connector: 'bookshelf',
       settings: {
-        host: env("DATABASE_HOST", "34.87.165.65"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 30144),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        client: 'postgres',
+        host: env(
+          'DATABASE_HOST',
+          'ec2-54-211-210-149.compute-1.amazonaws.com',
+        ),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dbt6bq99k206pc'),
+        username: env('DATABASE_USERNAME', 'zhfimwyvqhohsr'),
+        password: env(
+          'DATABASE_PASSWORD',
+          '143d36c75f2054e934f2613d91b2df2170c3256174cd69ffb6d6e7b7c8315b03',
+        ),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        ssl: env.bool("DATABASE_SSL", false),
-      },
+      options: {},
     },
   },
-});
+})
